1fc5c6bd038572c59ac2c58a4d32e398b4c51dc4
Jayden <your.email@example.comjayden@mendoza.im>
2026-05-31 00:06:17 -0400

    Fix base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/tronsoc-site/',
+  base: '/TRONSoc_TestSite/',
   plugins: [
     react(),
     tailwindcss(),
